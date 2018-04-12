4d0e3e2b5de156c8f411d09c04d4803f0dbf9d42
R.Otsubo <otsubo.seraku@gmail.com>
2018-04-12 11:26:59 +0900

    header almost finished.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//ナビメニュー
+    var navContainerWidth = $(".navContainer").outerWidth();
+    $(".navContainer").css("left",-navContainerWidth);
+$(".button").on("click",function(){
+  //非表示
+  if($(this).hasClass("active")){
+    $(".button-top").css("animation","button-top-back 1s forwards");
+    $(".button-bottom").css("animation"," button-bottom-back 1s forwards");
+    $(this).removeClass("active");
+    $(".navContainer").css("display","none");
+  }
+  //表示
+  else{
+    $(".button-top").css("animation","button-top 1s forwards");
+    $(".button-bottom").css("animation"," button-bottom 1s forwards");
+    $(this).addClass("active");
+    $(".navContainer").fadeIn(500).animate({"left":0},{duration:2000,queue:false})
+  }
+});
+
